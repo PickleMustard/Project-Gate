c90419efcafd4c443a586cee38dc2dc9003636a0
picklemustard <dillon.w.carter@gmail.com>
2024-12-30 22:15:57 -0600

    Fixing deprecations from RapidYAML update

--- src/yaml/yaml_parser.cpp
+++ src/yaml/yaml_parser.cpp
@@ -20,71 +20,67 @@
 #include <string>
 
 godot::YamlParser::YamlParser() {
-
 }
 
 godot::YamlParser::~YamlParser() {
-
 }
 
 void godot::YamlParser::_bind_methods() {
-  godot::ClassDB::bind_static_method("YamlParser", godot::D_METHOD("parse_file", "file"), &YamlParser::parse_file);
+	godot::ClassDB::bind_static_method("YamlParser", godot::D_METHOD("parse_file", "file"), &YamlParser::parse_file);
 }
 
 godot::Dictionary godot::YamlParser::parse_file(godot::String file) {
-  godot::JSON parse_result;
-  godot::Dictionary results;
-  ryml::Parser parser;
-  godot::String input_string = "";
-  godot::Ref<godot::FileAccess> fa = godot::FileAccess::open(file, FileAccess::READ);
-  while(!fa->eof_reached()) {
-    input_string += fa->get_line() + '\n';
-  }
-  godot::CharString temporary_string = input_string.utf8();
-  c4::substr text_string = ryml::to_substr(temporary_string.ptrw());
-  c4::yml::Tree tree = parser.parse_in_place("", text_string);
-  char buf[100000] {};
-  c4::substr output_json = buf;
-  ryml::emit_json(tree, output_json);
-  input_string = output_json.str;
-  input_string = input_string.substr(0, input_string.rfind("}") + 1);
-  UtilityFunctions::print(vformat("Input String: %s", input_string));
-  results = parse_result.parse_string(input_string);
-  fa->close();
-  return results;
+	godot::JSON parse_result;
+	godot::Dictionary results;
+	//ryml::Parser parser;
+	godot::String input_string = "";
+	godot::Ref<godot::FileAccess> fa = godot::FileAccess::open(file, FileAccess::READ);
+	while (!fa->eof_reached()) {
+		input_string += fa->get_line() + '\n';
+	}
+	godot::CharString temporary_string = input_string.utf8();
+	c4::substr text_string = ryml::to_substr(temporary_string.ptrw());
+	//c4::yml::Tree tree = parser.parse_in_place("", text_string);
+	c4::yml::Tree tree = ryml::parse_in_place("", text_string);
+	char buf[100000]{};
+	c4::substr output_json = buf;
+	ryml::emit_json(tree, output_json);
+	input_string = output_json.str;
+	input_string = input_string.substr(0, input_string.rfind("}") + 1);
+	UtilityFunctions::print(vformat("Input String: %s", input_string));
+	results = parse_result.parse_string(input_string);
+	fa->close();
+	return results;
 }
+/*
 void godot::YamlParser::test_yaml(godot::CharString text) {
 	ryml::Parser parser;
 	godot::JSON *parseResult = memnew(godot::JSON);
 	c4::substr text_string = ryml::to_substr(text.ptrw());
 	c4::yml::Tree tree = parser.parse_in_place("", text_string);
-  std::stringstream ss;
-  ss << ryml::as_json(tree);
-  std::string s = ss.str();
+	std::stringstream ss;
+	ss << ryml::as_json(tree);
+	std::string s = ss.str();
 	godot::Variant var;
 	//tree.rootref() >> var;
 	parseResult->set_data(var);
-  godot::String gs(s.c_str());
-  printf("Here\n");
+	godot::String gs(s.c_str());
+	printf("Here\n");
 
 	UtilityFunctions::print(vformat("Parser Output: %s", gs));
-  memdelete(parseResult);
-
+	memdelete(parseResult);
 }
-
 
 void godot::YamlParser::test_yaml_caller() {
-  godot::String input_string = "";
-  godot::Ref<godot::FileAccess> fa = godot::FileAccess::open("res://Configuration/Testing/test.yaml", FileAccess::READ);
-  while(!fa->eof_reached()) {
-    godot::String json_string = fa->get_line();
-    printf("Line read: %s\n", json_string.utf8().ptrw());
-    input_string += json_string + '\n';
-  }
-  CharStringT<char> ymlb = input_string.utf8();
-  printf("Final Input: %s\n", ymlb.ptr());
+	godot::String input_string = "";
+	godot::Ref<godot::FileAccess> fa = godot::FileAccess::open("res://Configuration/Testing/test.yaml", FileAccess::READ);
+	while (!fa->eof_reached()) {
+		godot::String json_string = fa->get_line();
+		printf("Line read: %s\n", json_string.utf8().ptrw());
+		input_string += json_string + '\n';
+	}
+	CharStringT<char> ymlb = input_string.utf8();
+	printf("Final Input: %s\n", ymlb.ptr());
 
-
-  YamlParser::test_yaml(ymlb);
-}
-
+	YamlParser::test_yaml(ymlb);
+}*/
