c71b1ec06031de86377a2f807add0d2291eae4fc
picklemustard <dillon.w.carter@gmail.com>
2025-01-23 21:31:33 -0600

    Grabbing all index files from content packs

--- project-gate/Assets/Scripts/AssetLoading/AssetLoader.cs
+++ project-gate/Assets/Scripts/AssetLoading/AssetLoader.cs
@@ -1,7 +1,46 @@
 using System.Collections.Generic;
 using Godot.Collections;
+using System.Linq;
+using Godot;
 public class AssetLoader
 {
 
-  List<>
+  /* TODO:
+   * The basegame loader needs to navigate through the repository to the Content-Packs directory
+   * From there, it needs to parse through each of the content packs to load the values into the game
+   *
+   * Currently, grab the current directory and work to the 1st Content pack
+   */
+  public void LoadBaseContent()
+  {
+    DirAccess workingDirectory = DirAccess.Open("res://");
+    string[] dirs = workingDirectory.GetDirectories();
+    foreach (string d in dirs)
+    {
+      GD.Print(d);
+    }
+#if TOOLS
+    GD.Print("Tools");
+    workingDirectory.ChangeDir("Assets");
+    string BaseDirectory = workingDirectory.GetCurrentDir();
+    GD.Print("Base Directory: " + BaseDirectory);
+#else
+    string BaseDirectory = workingDirectory.GetCurrentDir() + "Content-Packs/";
+#endif
+    AssetFileLocationDiscoverer.DiscoverFileLocation(BaseDirectory);
+
+    /*string testReadWeapons = BaseDirectory + "/Configured-Assets/Weapons/";
+
+    GodotObject yamlParser = ClassDB.Instantiate("YamlParser").AsGodotObject();
+    string toLoad = testReadWeapons + "index.yml";
+    GD.Print(toLoad);
+    Dictionary parsedData = (Dictionary)yamlParser.Call("parse_file", toLoad);
+
+    GD.Print(parsedData);
+    Array definitions = (Array)parsedData["definitions"];
+    GD.Print(definitions[0]);
+    Dictionary indexItem = (Dictionary)definitions[0];
+    GD.Print(string.Format("File Exists: {0}, {1}", testReadWeapons + indexItem["file"], FileAccess.FileExists(testReadWeapons + indexItem["file"])));
+    */
+  }
 }
--- project-gate/Assets/Scripts/Initialization/ProgramInitialization.cs
+++ project-gate/Assets/Scripts/Initialization/ProgramInitialization.cs
@@ -2,4 +2,8 @@ using Godot;
 
 public partial class ProgramInitialization : Node {
   //Import Assets then move onto Main Menu
+  public override void _Ready() {
+    AssetLoader al = new AssetLoader();
+    al.LoadBaseContent();
+  }
 }
